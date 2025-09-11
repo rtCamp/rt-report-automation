0c0f7bb9ce577a5fb21ed62cf9357487d6836abb
Kallyan Singha <singhakallyan@gmail.com>
2025-09-11 17:57:14 +0530

    feat: added GraphQl issue and filter search query

---
+++ app/github/query/gql_queries.py
@@ -0,0 +1,114 @@
+from app.github.utils.helpers import format_to_yymmdd
+
+
+def get_issue_fetch_query(comments: bool = False) -> str:  # noqa: FBT001, FBT002 By default the issue comments are disabled in this gql query.
+	"""
+	Generates a GraphQL query string to fetch GitHub issues.
+
+	Args:
+	comments (bool): If True, the query will fetch the first 100 comments.
+	Defaults to False.
+
+	Returns:
+	str: A GraphQL query string.
+	"""
+	return f"""
+    query ($search_query: String!, $after: String) {{
+      search(query: $search_query, type: ISSUE, first: 100, after: $after) {{
+        pageInfo {{
+          hasNextPage
+          endCursor
+        }}
+        nodes {{
+          ... on Issue {{
+            number
+            title
+            state
+            url
+            updatedAt
+            repository {{
+              owner {{
+                login
+              }}
+              name
+            }}
+            labels(first: 50) {{
+              items: nodes {{
+                name
+              }}
+            }}
+            crossReferencedPRs: timelineItems(
+              first: 50
+              itemTypes: [CROSS_REFERENCED_EVENT]
+            ) {{
+              items: nodes {{
+                ... on CrossReferencedEvent {{
+                  source {{
+                    ... on PullRequest {{
+                      pr_id: number
+                      title
+                      url
+                    }}
+                  }}
+                }}
+              }}
+            }}
+            projectItems(first: 50) {{
+              items: nodes {{
+                id
+                project {{
+                  title
+                  number
+                }}
+                fieldValues(first: 50) {{
+                  items: nodes {{
+                    # Single-select fields (e.g. Status, Priority, etc.)
+                    ... on ProjectV2ItemFieldSingleSelectValue {{
+                      name
+                      field {{
+                        ... on ProjectV2SingleSelectField {{
+                          name
+                        }}
+                      }}
+                    }}
+                  }}
+                }}
+              }}
+            }}
+            {
+		'''
+            comments(first: 100) {
+              items: nodes {
+                author {
+                  login
+                }
+                body
+                createdAt
+                updatedAt
+                url
+              }
+            }
+            '''
+		if comments
+		else ""
+	}
+          }}
+        }}
+      }}
+    }}
+    """
+
+
+def get_issue_search_query(
+	owner: str,
+	repo: str,
+	start_date: str,
+	end_date: str,
+) -> str:
+	query = "repo:{owner}/{repo} is:issue updated:{startDate}..{endDate}"
+	return query.format(
+		owner=owner,
+		repo=repo,
+		startDate=format_to_yymmdd(start_date),
+		endDate=format_to_yymmdd(end_date),
+	)
