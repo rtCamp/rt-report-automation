2deb047450d59e4c7f086717e1f4bd5ee6ec1799
Nirav sherasiya <sherasiyanirav@gmail.com>
2025-12-26 13:32:54 +0530

    Remove: unused constant

--- app/github/utils/constants.py
+++ app/github/utils/constants.py
@@ -11,6 +11,3 @@ GITHUB_SERVICE_FAILURE_MAX_RETRY_LIMIT = 3
 
 # GitHub Data service API rate limit per minute
 GITHUB_API_RATE_LIMIT = 10
-
-# Default value for missing fields
-DEFAULT_VALUE = "N/A"
